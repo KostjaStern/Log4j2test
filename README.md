b76cdf7736b29393fc0ae074bea626aaee721d8b
KostjaStern <kostjastern@gmail.com>
2019-10-19 23:44:00 +0300

    Log4j2 - simple example

---
+++ README.md
@@ -0,0 +1,10 @@
+# Log4j2test
+The simple example of Log4j2 usage.
+
+```bash
+   >mvn clean
+   >mvn package
+   >cd target
+   >java -jar log4j2-test-0.1.jar
+```
+
---
+++ src/com/sternkn/log4j2/App.java
@@ -0,0 +1,16 @@
+package com.sternkn.log4j2;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class App
+{
+   private static final Logger logger = LogManager.getLogger(App.class.getName());
+   
+   public static void main(String[] args)
+   {
+      logger.info("test");
+      System.out.println( "Hello World!" );
+      logger.warn("Dummy warning");
+   }
+}
